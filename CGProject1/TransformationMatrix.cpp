ebe042feee28ad95035f29730d6822fc1cdca47f
ChandlorRatcliffe <47456805+ChandlorRatcliffe@users.noreply.github.com>
2021-04-12 19:18:00 -0500

    Refactored Code for better variable names
    Minor mistakes were corrected along the way, and a typo in the comments was corrected.

--- CGProject1/TransformationMatrix.cpp
+++ CGProject1/TransformationMatrix.cpp
@@ -5,43 +5,44 @@
  * Instatiate a new TransformationMatrix as an identity matrix.
  */
 TransformationMatrix::TransformationMatrix() {
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j)
-			this->matrix[i][j] = float(i == j);
+	for (int row = 0; row < 3; ++row)
+		for (int column = 0; column < 3; ++column)
+			this->matrix[row][column] = float(row == column);
 	this->type = IDENTITY;
 }
 /**
  * Take a vector float[2] by reference, and modify it to contain
  * the result of the application of the transformation matrix to it.
  */
-void TransformationMatrix::applyTo(float v[2]) {
+void TransformationMatrix::applyTo(float vector[2]) {
 	//Extend the vector
-	float h[3][1] = { {v[0]}, {v[1]}, {1.0} };
+	float homogeneousVector[3][1] = { {vector[0]}, {vector[1]}, {1.0} };
 	//Create a place for the result
-	float r[3][1] = { {0},{0},{0} };
+	float product[3][1] = { {0},{0},{0} };
 	//Perform the multiplication
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 1; ++j)
-			for (int k = 0; k < 3; ++k)
-				r[i][j] += this->matrix[i][j] * h[i][j];
+	for (int row = 0; row < 3; ++row)
+		for (int column = 0; column < 1; ++column)
+			for (int common = 0; common < 3; ++common)
+				product[row][column] += this->matrix[row][common] * homogeneousVector[common][column];
 	//extract the result, and store in the given vector
-	v[0] = h[0][0];
-	v[1] = h[1][0];
+	vector[0] = product[0][0];
+	vector[1] = product[1][0];
 }
 /**
- * Compose this Transformatrix (A) with another TransformationMatrix (B)
+ * Compose this TransformationMatrix (A) with another TransformationMatrix (B)
  * such that the result of AB is stored in this TransofrmationMatrix.
  */
-void TransformationMatrix::composeWith(TransformationMatrix *T) {
+void TransformationMatrix::composeWith(TransformationMatrix *other) {
 	//Create a place for the result
-	float r[3][3] = { {0.0,0.0,0.0},{0.0,0.0,0.0},{0.0,0.0,0.0} };
+	float product[3][3] = { {0.0,0.0,0.0},{0.0,0.0,0.0},{0.0,0.0,0.0} };
 	//Perform the multiplication
-	for(int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j)
-			for (int k = 0; k < 3; ++k)
-				r[i][j] += this->matrix[i][j] * T->matrix[i][j];
+	for(int row = 0; row < 3; ++row)
+		for (int column = 0; column < 3; ++column)
+			for (int common = 0; common < 3; ++common)
+				product[row][column] += this->matrix[row][common] * other->matrix[common][column];
 	//Store the result in this TransformationMatrix
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j)
-			this->matrix[i][j] = r[i][j];
+	for (int row = 0; row < 3; ++row)
+		for (int column = 0; column < 3; ++column)
+			this->matrix[row][column] = product[row][column];
+    this->type = COMPOSITE;
 }
