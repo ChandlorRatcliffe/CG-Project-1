39213c376d187e919449b1550614f32a3bef859e
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2021-04-13 10:29:31 -0500

    Added ShearYMatrix. Corrected ShearXMatrix.

--- CGProject1/ShearYMatrix.cpp
+++ CGProject1/ShearYMatrix.cpp
@@ -1,28 +1,28 @@
-#include "ShearXMatrix.h"
-ShearXMatrix::ShearXMatrix(float sx) {
+#include "ShearYMatrix.h"
+ShearYMatrix::ShearYMatrix(float sy) {
 	this->matrix[0][0] = 1.0;
-	this->matrix[0][1] = sx;
+	this->matrix[0][1] = 0.0;
 	this->matrix[0][2] = 0.0;
-	this->matrix[1][0] = 0.0;
+	this->matrix[1][0] = sy;
 	this->matrix[1][1] = 1.0;
 	this->matrix[1][2] = 0.0;
 	this->matrix[2][0] = 0.0;
 	this->matrix[2][1] = 0.0;
 	this->matrix[2][2] = 1.0;
-	this->type = Type::SHEARX;
+	this->type = Type::SHEARY;
 }
-ShearXMatrix::ShearXMatrix(float sx, float yAxis) {
+ShearYMatrix::ShearYMatrix(float sy, float xAxis) {
 	this->matrix[0][0] = 1.0;
-	this->matrix[0][1] = sx;
+	this->matrix[0][1] = 0.0;
 	this->matrix[0][2] = 0.0;
-	this->matrix[1][0] = 0.0;
+	this->matrix[1][0] = sy;
 	this->matrix[1][1] = 1.0;
 	this->matrix[1][2] = 0.0;
 	this->matrix[2][0] = 0.0;
 	this->matrix[2][1] = 0.0;
 	this->matrix[2][2] = 1.0;
-	this->type = Type::SHEARX;
-	TranslationMatrix T = TranslationMatrix(0.0, -yAxis);
+	this->type = Type::SHEARY;
+	TranslationMatrix T = TranslationMatrix(-xAxis,0.0);
 	this->composeWith(&T);
 
 }
